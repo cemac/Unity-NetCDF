d9d994b3d1452e86e0cd38b624cebddf1755c6f1
Anne Barber <lmkk419@leeds.ac.uk>
2024-11-12 10:25:35 +0000

    Tidied code for rainbow_cube.cs and updated README

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ This repository contains some basic C# and Shaderlab scripts for reading and dis
 ### Available scripts
 - `CreateMesh.cs` - contains code for generating a variety of simple empty meshes in Unity (ranging from a single triangle to a 3D cube)
 - `VertexColorShader.shader` - can be used to create a custom shader in Unity such that an RGBA colour is assigned to a vertex based on its position within a mesh
-- `rainbow_cube.cs` - contains functions to read a NetCDF file in Unity and print statistics to the console, then construct a 3D mesh based on the file dimensions and assign a semi-random RGBA colour to each vertex based on the custom shader 'VertexColorShader.shader'
+- `rainbow_cube.cs` - contains a function to construct a 3D mesh based on user-defined dimensions, and assign a semi-random RGBA colour to each vertex based on the custom shader 'VertexColorShader.shader'
 - `Wind_Mesh.cs` - contains code to read in a remote NetCDF file and plot the horizontal wind field onto a 3D mesh (by assigning the data values to the mesh vertex) using the custom vertex shader 'VertexColorShader.shader'
 - `netcdf_mesh_update.cs` - contains code to read in a local NetCDF file (user must provide their own), construct a 3D mesh and plot data onto it. This script contains functionality to move forward and backward in time using the keyboard arrow keys. This script was developed using cloud fraction data from a WRF file but can be modified for other types of data.
 - `MultipleFields/wrf_multi_fields.cs` - contains code similar to `netcdf_mesh_updates.cs` but includes additional 'toggle' functionality to switch between different meteorological fields on-the-fly
@@ -25,10 +25,11 @@ Most of these scripts use third-party libraries to read NetCDF files using C#. T
 Download these packages to your Assets/Plugins folder within your Unity project.
 
 ### How to use the scripts
-- Save the scripts under Assets/Scripts and the shader under Assets/Shaders in your Unity project
-- Double-click on the script in the Project panel to edit as appropriate
-- If making use of the custom vertex shader, create a new material under Assets/Materials and assign the custom shader to the material 
-- Create a new object in the Hierarchy panel
+- Create a new project using Unity Editor (select 3D Built-In Render Pipline or Universal 3D)
+- In the Projects area, navigate to your Assets and save the script(s) into a folder called Scripts and the shader into a folder called Assets (if these folders don't exist already, create them)
+- Double-click on the script in the Project panel to edit as required. You may need to set your default script editor (IDE) in Unity if you have not done this already.
+- If making use of the custom vertex shader, create a new material under Assets/Materials (again, create the folder if it doesn't already exist) and assign the custom shader to the material 
+- Create a new object in the Hierarchy panel (by right-clicking, and selecting 'Create Empty')
 - Add components to the new object: a mesh filter, a mesh renderer, and a C# script (link the appropriate script)
 - If making use of the custom vertex shader, change the mesh renderer from its default material to the custom material you made earlier
 - During these steps, you will see Unity attempt to compile the code. Check the Project/Console section to see if there are any errors in compilation.
--- rainbow_cube.cs
+++ rainbow_cube.cs
@@ -1,14 +1,6 @@
-// This script contains the following functions:
-// 1. "ReadData": Read in a netcdf file and prints some stats to the Unity console. 
-// Extract a variable as a 4D array and remove the time dimension to create a 3d array.
-// 2. "GenerateMesh": Create a 3D mesh with the same dimensions as the 3d array from our 
-// file and assign a semi-random RGB color to each vertex based on its position. Eventually 
-// we want to assign the values of the 3d array to the mesh vertices.
-// This script is currently written for use with a specific dataset, but can be used as inspiration for other datasets.
-
-// This script makes use of the following libraries which must be installed in Unity's Plugins:
-// SDSLite (https://www.nuget.org/packages/SDSLite)
-// netCDF-C (https://docs.unidata.ucar.edu/netcdf-c/current/winbin.html)
+// This script contains a single function "GenerateMesh" that creates a 3D mesh with dimensions 
+// xGridSize, yGridSize and zGridSize (defined by the user at the start of the script) and assigns a semi-random RGB
+// color to each vertex based on its x,y,z position.
 
 // Add namespaces
 using System.Collections;
@@ -22,7 +14,7 @@ using System.Linq;
 public class rainbow_cube : MonoBehaviour
 
 {
-    // Grid dimensions - change these to appropriate values for your dataset
+    // Grid dimensions - change these to integer values of your choosing
     int xGridSize = 4;
     int yGridSize = 4;
     int zGridSize = 4;
@@ -30,64 +22,11 @@ public class rainbow_cube : MonoBehaviour
     // When the program starts, execute the following
     void Start()
     {
-        // Read in data and print some stats
-        ReadData();
-
-        // Create empty mesh for a single timeframe and assign RGB values to vertices
+        // Create empty mesh and assign RGB values to vertices
         GenerateMesh();
 
-        // Assign array values to mesh vertices - haven't written this function yet
-        //AssignValues();
-
     }
-    // This function will open a local netcdf file and print some stats to the console
-    void ReadData()
-    {
-        // Open the file (change the path to the location of your netcdf file)
-        using (DataSet ds = DataSet.Open("C:/Users/lmkk419/Downloads/sresa1b_ncar_ccsm3-example.nc")){
 
-            // Print some general info about the dataset to the console
-            Debug.Log(ds);
-
-            // Get some stats for each variable. Change the variable name to the one you want to extract
-            var lat = ds.GetData<float[]>("lat");
-            Debug.Log($"Latitude: len={lat.Length}, min={lat.Min()}, max={lat.Max()}");
-
-            var lon = ds.GetData<float[]>("lon");
-            Debug.Log($"Longitude: len={lon.Length}, min={lon.Min()}, max={lon.Max()}");
-
-            var plev = ds.GetData<double[]>("plev");
-            Debug.Log($"Pressure levels: len={plev.Length}, min={plev.Min()}, max={plev.Max()}");
-
-            // For variables with 4 dimensions, specify using [,,,]
-            var ua_4d = ds.GetData<float[,,,]>("ua");
-            Debug.Log($"ua (horizontal wind): len={ua_4d.Length}");
-            
-            // If we want to plot a 4D variable, we'll need to extract a single time slice.
-            // My dataset only had one time slice, so I'll remove the time dimension from ua_4d
-            float[,,] ua_3d = new float[zGridSize,yGridSize,xGridSize];
-
-            // Copy the data from the 4D array to the 3D array, ignoring the time dimension
-            for (int i = 0; i < zGridSize; i++)
-            {
-                for (int j = 0; j < yGridSize; j++)
-                {
-                    for (int k = 0; k < xGridSize; k++)
-                    {
-                        ua_3d[i, j, k] = ua_4d[0, i, j, k];
-                    }
-                }
-            }
-
-            // Check what ua_3d looks like
-            Debug.Log($"z length of ua_3d array: {ua_3d.GetLength(0)}"); 
-            Debug.Log($"y length of ua_3d array: {ua_3d.GetLength(1)}"); 
-            Debug.Log($"x length of ua_3d array: {ua_3d.GetLength(2)}");
-
-        } // close dataset
-    } // end of function
-
-    // This function will create a mesh with xGridSize * yGridSize * zGridSize dimensions
     void GenerateMesh()
     {
         // Create a new mesh using the Mesh function
@@ -123,7 +62,7 @@ public class rainbow_cube : MonoBehaviour
             }
         }
 
-        // Assign colours to each vertex by looping over them - could probably move this loop inside the previous loop?
+        // Assign colours to each vertex by looping over them
         // Note that you will need to create a custom shader called VertexColorShader.shader, assign it to a new material and
         // then colourise your object by assigning the new material to the object's mesh renderer
         for (int i = 0; i < vertices.Length; i++)
@@ -145,6 +84,7 @@ public class rainbow_cube : MonoBehaviour
             
             // Assign the colour to the vertex
             colors[i] = new Color(x_rand, y_rand, z_rand);
+            
         }
 
         // Define the triangles
@@ -224,7 +164,6 @@ public class rainbow_cube : MonoBehaviour
             vert += xGridSize + 1;
         }
     
-   
         // Assign vertices, triangles and colours to the mesh
         mesh.vertices = vertices;
         mesh.triangles = triangles;
@@ -249,16 +188,9 @@ public class rainbow_cube : MonoBehaviour
         
     }
 
-    /*
-    void AssignValues()
-    {
-        // Assign values from the 3D array to the mesh vertices
-        // This will be done during the mesh creation process
-    }
-    */
-
     // Update is called once per frame
     void Update()
     {  
+        // We don't call anything in the update functino
     }
 }
